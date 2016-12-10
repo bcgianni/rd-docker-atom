9e6bd1c3fa04348baf9df61292d2821947d510ee
Caio <caio.bonfante@resultadosdigitais.com.br>
2016-12-10 11:05:17 -0200

    improve installation script

--- install.sh
+++ install.sh
@@ -1,1 +1,22 @@
-docker build -t rd-docker-atom . && cp rd-atom /usr/local/bin
+BIN_PATH="/usr/local/bin/rd-atom"
+DOWNLOAD_URL="https://raw.githubusercontent.com/ResultadosDigitais/rd-docker-atom/master/rd-atom"
+
+build_dockerfile() {
+  docker build -t rd-docker-atom .
+}
+
+download_executable() {
+  curl -o "$BIN_PATH" "$DOWNLOAD_URL"
+  chmod +x "$BIN_PATH"
+}
+
+validate_user_permission() {
+  if [ $(id -u) -ne 0 ] ; then
+    echo  "Please run as root"
+    exit 1
+  fi
+}
+
+validate_user_permission
+build_dockerfile
+download_executable
